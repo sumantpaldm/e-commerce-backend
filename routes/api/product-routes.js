ae0aaec2e2d9bb53989c848fc79d48f03d5d6065
SINGH 1258 <sumantpaldm@gmail.com>
2021-11-17 06:53:21 -0500

    added video

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -57,6 +57,7 @@ router.post("/", (req, res) => {
     price: req.body.price,
     stock: req.body.stock,
     tagIds: req.body.tagIds,
+    category_id: req.body.category_id
   })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
